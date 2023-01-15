018558e7a44ec543d59883d7902d6b71466eb36a
Robert Trainor <blashframework@gmail.com>
2023-01-14 18:34:51 -0600

    Added a config

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+Config.Menus = {
+    main_menu = {
+        welcome = {
+            header = 'Welcome',
+            message = 'Thanks for joining our server!'
+        },
+        information = {
+            { header = 'Discord', message = 'discord.gg/blashframework' }
+        }
+    }
+}
